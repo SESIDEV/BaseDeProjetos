048358251b217c8049cd38c7870c8f14e47f6db4
Bloutware <iurykoz@gmail.com>
2022-03-23 16:52:19 -0300

    Add Logo

--- BaseDeProjetos/Models/Empresa.cs
+++ BaseDeProjetos/Models/Empresa.cs
@@ -5,12 +5,19 @@ namespace BaseDeProjetos.Models
     public class Empresa
     {
         public virtual int Id { get; set; }
+        
         [Display(Name = "Nome da Empresa")]
         public virtual string Nome { get; set; }
+        
+        [Display(Name = "Logo da Empresa (Link URL)")]
+        public virtual string Logo { get; set; }
+        
         [Display(Name = "CNPJ da Empresa")]
         public virtual string CNPJ { get; set; }
+        
         [Display(Name = "Segmento da Empresa")]
         public virtual string Segmento { get; set; }
+        
         [Display(Name = "Estado da Empresa")]
         public virtual string Estado { get; set; }
 
