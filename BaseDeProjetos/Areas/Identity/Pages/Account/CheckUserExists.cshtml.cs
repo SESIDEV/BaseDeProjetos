6115789af535eb1448836151a211193d9178ffe0
Rafael Magno - Firjan <rafaelmagno125@gmail.com>
2022-06-03 11:50:24 -0300

    Verificação se o usuário existe na solicitação de recuperação de senha.

---
+++ BaseDeProjetos/Areas/Identity/Pages/Account/CheckUserExists.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaseDeProjetos.Areas.Identity.Pages.Account
+{
+    public class CheckUserExistsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
