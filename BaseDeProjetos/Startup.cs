ddda7bad7ba9a0cc760a8a04c3b7383df7285fe9
LeonNasc <l.nasc@live.com>
2022-07-26 15:07:23 -0300

    Socorro

--- BaseDeProjetos/Startup.cs
+++ BaseDeProjetos/Startup.cs
@@ -38,8 +38,8 @@ namespace BaseDeProjetos
 
                 // This line split "server=localhost:[port]" in "server=localhost;port=[port]
                 ConStr = ConStr.Replace(":", ";port=");
+                System.Console.WriteLine("ConStr");
 
-                throw new System.Exception(ConStr);
 
                 services.AddDbContext<ApplicationDbContext>(options =>
                     options.UseMySql(ConStr).UseLazyLoadingProxies());
