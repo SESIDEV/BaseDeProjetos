4248dacbeb2c5486f0c0e12fe6842ec1967c3a8a
LeonNasc <l.nasc@live.com>
2022-07-26 14:31:07 -0300

    null

--- BaseDeProjetos/Startup.cs
+++ BaseDeProjetos/Startup.cs
@@ -15,94 +15,102 @@ using System.Globalization;
 
 namespace BaseDeProjetos
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-    public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      if (System.Environment.GetEnvironmentVariable("Ambiente") == "Web")
-      {
-                throw new System.Exception(System.Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb").ToString());
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseMySql(
-         System.Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb").ToString()).UseLazyLoadingProxies());
-      }
-      else
-      {
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseMySql(
-        Configuration.GetConnectionString("DefaultConnection")).UseLazyLoadingProxies());
-      }
-      services.AddDefaultIdentity<Usuario>(options => options.SignIn.RequireConfirmedAccount = true)
-          .AddEntityFrameworkStores<ApplicationDbContext>();
-      services.AddDistributedMemoryCache();
-      services.AddLocalization(options => options.ResourcesPath = "Resources");
-      services.Configure<RequestLocalizationOptions>(
-          options =>
-              {
-                var supportedCultures = new List<CultureInfo>
-                  {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (System.Environment.GetEnvironmentVariable("Ambiente") == "Web")
+            {
+                string ConStr = System.Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb");
+                ConStr = ConStr.Replace("Database", "database");
+                ConStr = ConStr.Replace("Data Source", "server");
+                ConStr = ConStr.Replace("User Id", "user");
+                ConStr = ConStr.Replace("Password", "password");
+                ConStr = ConStr.Replace("127.0.0.1", "localhost");
+
+                // This line split "server=localhost:[port]" in "server=localhost;port=[port]
+                ConStr = ConStr.Replace(":", ";port=");
+
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseMySql(ConStr).UseLazyLoadingProxies());
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseMySql(
+                Configuration.GetConnectionString("DefaultConnection")).UseLazyLoadingProxies());
+            }
+            services.AddDefaultIdentity<Usuario>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDistributedMemoryCache();
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.Configure<RequestLocalizationOptions>(
+                options =>
+                    {
+                        var supportedCultures = new List<CultureInfo>
+                        {
                             new CultureInfo("pt-BR"),
-                  };
+                        };
 
-                options.DefaultRequestCulture = new RequestCulture(culture: "pt-BR", uiCulture: "pt-BR");
-                options.SupportedCultures = supportedCultures;
-                options.SupportedUICultures = supportedCultures;
-              });
+                        options.DefaultRequestCulture = new RequestCulture(culture: "pt-BR", uiCulture: "pt-BR");
+                        options.SupportedCultures = supportedCultures;
+                        options.SupportedUICultures = supportedCultures;
+                    });
 
-      services.AddTransient<IEmailSender, EmailSender>();
+            services.AddTransient<IEmailSender, EmailSender>();
 
-      services.AddSession();
-      services.AddControllersWithViews();
-      services.AddRazorPages();
+            services.AddSession();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
 
-      services.Configure<CookiePolicyOptions>(options =>
-      {
-        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-        options.CheckConsentNeeded = context => true;
-        options.MinimumSameSitePolicy = SameSiteMode.None;
-      });
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+          // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+          options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseSession();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+
+            context.Database.Migrate();
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseDatabaseErrorPage();
-      }
-      else
-      {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
-
-      app.UseRouting();
-      app.UseSession();
-
-      app.UseAuthentication();
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllerRoute(
-                  name: "default",
-                  pattern: "{controller=Home}/{action=Index}/{id?}");
-        endpoints.MapRazorPages();
-      });
-
-      context.Database.Migrate();
-    }
-  }
 }
