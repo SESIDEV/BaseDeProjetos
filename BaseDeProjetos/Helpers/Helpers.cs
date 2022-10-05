ac0227cd1794cabd032cba12e8b65c9d7b5de962
LeonNasc <l.nasc@live.com>
2022-10-05 18:03:49 -0300

    Ajustes datas

--- BaseDeProjetos/Helpers/Helpers.cs
+++ BaseDeProjetos/Helpers/Helpers.cs
@@ -1,7 +1,30 @@
-﻿namespace BaseDeProjetos.Helpers
+﻿using System.Drawing;
+
+namespace BaseDeProjetos.Helpers
 {
-    public class Helpers
+    public static class Helpers
     {
+        public static string FormatarValoresDashboards(decimal valor)
+        {
+            if (valor >= 100000000)
+            {
+                return (valor / 1000000).ToString("0.# milhões");
+            }
+            if (valor >= 1000000)
+            {
+                return (valor / 1000000).ToString("0.## milhões");
+            }
+            if (valor >= 100000)
+            {
+                return (valor / 1000).ToString("0.# mil");
+            }
+            if (valor >= 10000)
+            {
+                return (valor / 1000).ToString("0.## mil");
+            }
+
+            return valor.ToString("#,0");
+        }
     }
 
 }
