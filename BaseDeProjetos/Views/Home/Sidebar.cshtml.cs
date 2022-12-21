a89a05698d39d1d086aafbd320ff119b9bf049b6
hhenriques1999 <hhenriques1999@gmail.com>
2022-12-21 14:22:31 -0300

    feat: :sparkles: Criar partial view para a sidebar

---
+++ BaseDeProjetos/Views/Home/Sidebar.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaseDeProjetos.Views.Home
+{
+    public class SidebarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
