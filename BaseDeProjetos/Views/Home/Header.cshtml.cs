a17245785fd54a4262ec7d2e9d0c74c3b948ef2f
hhenriques1999 <hhenriques1999@gmail.com>
2022-12-21 14:22:51 -0300

    feat: :sparkles: Criar partial view para o header

---
+++ BaseDeProjetos/Views/Home/Header.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaseDeProjetos.Views.Home
+{
+    public class HeaderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
