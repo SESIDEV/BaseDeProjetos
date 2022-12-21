c98eb8feff3c4eb3dbfcb99570df6909ba215df6
hhenriques1999 <hhenriques1999@gmail.com>
2022-12-21 17:43:17 -0300

    feat: :boom: Jogar o Header no Layout
    Quebra as views no momento, exceto a loggedinhome

--- BaseDeProjetos/Views/Shared/Header.cshtml.cs
+++ BaseDeProjetos/Views/Shared/Header.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BaseDeProjetos.Views.Home
+namespace BaseDeProjetos.Views.Shared
 {
     public class HeaderModel : PageModel
     {
