3a57afe3b2b95fd807c3fadeab76dfadbcff284f
hhenriques1999 <hhenriques1999@gmail.com>
2023-01-06 19:52:18 -0300

    fix: :bug: Fixar bug de funções do Bootstrap 4->5
    Explicação: https://www.sitepoint.com/bootstrap-5-new-features-examples/#changeindataattributes

--- BaseDeProjetos/wwwroot/js/app.js
+++ BaseDeProjetos/wwwroot/js/app.js
@@ -2,7 +2,7 @@
 
 /* ===== Enable Bootstrap Popover (on element  ====== */
 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
