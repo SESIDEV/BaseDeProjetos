bb8b95ecbc28f97d04ef8de2d6861851149eff53
hhenriques1999 <hhenriques1999@gmail.com>
2023-01-19 12:01:19 -0300

    feat(editais): :sparkles: Permitir que se crie uma submissão de edital no Index de Editais (placeholder)

---
+++ BaseDeProjetos/ViewComponents/ModalCreateSubmissaoEditalViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using BaseDeProjetos.Data;
+using Microsoft.AspNetCore.Mvc;
+using BaseDeProjetos.Models;
+using System.Threading.Tasks;
+
+namespace BaseDeProjetos.ViewComponents
+{
+    public class ModalCreateSubmissaoEditalViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ModalCreateSubmissaoEditalViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(new Submissao());
+        }
+
+    }
+}
