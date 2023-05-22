e88724a33ca0330e86bc02c1e0d588e5c269bb3b
hhenriques1999 <hhenriques1999@gmail.com>
2023-05-22 15:35:46 -0300

    feat(projetos): :sparkles: Incluir novo Model para indicadores de projeto

--- BaseDeProjetos/Models/Projeto.cs
+++ BaseDeProjetos/Models/Projeto.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 
 namespace BaseDeProjetos.Models
 {
     public class Projeto
     {
+        public Projeto() 
+        { 
+        
+        }
+
+        public Projeto(ProjetoIndicadores indicador)
+        {
+            Indicadores = new List<ProjetoIndicadores> { indicador };
+        }
+
         [Key]
         [Display(AutoGenerateFilter = true)]
         public virtual string Id { get; set; }
@@ -22,9 +30,9 @@ namespace BaseDeProjetos.Models
         public virtual LinhaPesquisa AreaPesquisa { get; set; }
 
         [Display(AutoGenerateFilter = true, Name = "Data de Início")]
-        public virtual DateTime DataInicio { get; set; }
+        public virtual DateTime DataInicio { get; set; } = DateTime.Now;
 
-        public virtual DateTime DataEncerramento { get; set; }
+        public virtual DateTime DataEncerramento { get; set; } = DateTime.Now;
 
         [Display(AutoGenerateFilter = true, Name = "Membros da Equipe")]
         public virtual string MembrosEquipe { get; set; }
@@ -43,10 +51,14 @@ namespace BaseDeProjetos.Models
         [Display(AutoGenerateFilter = true, Name = "Duração do Projeto em meses")]
         public virtual int DuracaoProjetoEmMeses { get; set; }
 
+        [Display(AutoGenerateFilter = true, Name = "Valor Total do Projeto")]
         public virtual double ValorTotalProjeto { get; set; }
 
+        [Display(AutoGenerateFilter = true, Name = "Valor de Aporte de Recursos")]
         public virtual double ValorAporteRecursos { get; set; }
 
         public virtual Instituto Casa { get; set; }
+
+        public virtual List<ProjetoIndicadores> Indicadores { get; set; } = new List<ProjetoIndicadores> { new ProjetoIndicadores() };
     }
 }
---
+++ BaseDeProjetos/Models/ProjetoIndicadores.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BaseDeProjetos.Models
+{
+    public class ProjetoIndicadores
+    {
+        [Key]
+        [Display(Name = "Id do indicador do projeto")]
+        public virtual string Id { get; set; }
+
+        [ForeignKey("IdProjeto")]
+        public virtual Projeto Projeto { get; set; }
+
+        public virtual string IdProjeto { get; set; }
+
+        [Display(Name = "Regramento")]
+        public virtual bool Regramento { get; set; }
+        
+        [Display(Name = "Repasse")]
+        public virtual bool Repasse { get; set; }
+
+        [Display(Name = "Abertura de SC (Serviço)")]
+        public virtual bool ComprasServico { get; set; }
+
+        [Display(Name = "Abertura de SC (Material)")]
+        public virtual bool ComprasMaterial { get; set; }
+
+        [Display(Name = "Contratação de Bolsista")]
+        public virtual bool Bolsista { get; set; }
+
+        [Display(Name = "Questionário de Satisfação na Metade do Projeto")]
+        public virtual bool SatisfacaoMetadeProjeto { get; set; }
+
+        [Display(Name = "Questionário de Satisfação no Fim do Projeto")]
+        public virtual bool SatisfacaoFimProjeto { get; set; }
+
+        [Display(Name = "Relatórios")]
+        public virtual bool Relatorios { get; set; }
+
+        [Display(Name = "Prestação de Contas")]
+        public virtual bool PrestacaoContas { get; set; }        
+    }
+}
