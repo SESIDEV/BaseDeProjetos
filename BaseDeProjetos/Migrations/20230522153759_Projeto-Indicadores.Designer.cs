8eee211df4980cb121478f72566771adfa3531f3
hhenriques1999 <hhenriques1999@gmail.com>
2023-05-22 15:35:07 -0300

    build(migration): :fire: Migration para a implementação de indicadores de projetos
    Cuidado!

--- BaseDeProjetos/Migrations/20230522153759_Projeto-Indicadores.Designer.cs
+++ BaseDeProjetos/Migrations/20230522153759_Projeto-Indicadores.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BaseDeProjetos.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BaseDeProjetos.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230522153759_Projeto-Indicadores")]
+    partial class ProjetoIndicadores
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -107,16 +109,10 @@ namespace BaseDeProjetos.Migrations
                     b.Property<int>("Estado")
                         .HasColumnType("int");
 
-                    b.Property<bool>("Industrial")
-                        .HasColumnType("tinyint(1)");
-
                     b.Property<string>("Logo")
                         .HasColumnType("longtext CHARACTER SET utf8mb4");
 
                     b.Property<string>("Nome")
-                        .HasColumnType("longtext CHARACTER SET utf8mb4");
-
-                    b.Property<string>("NomeFantasia")
                         .HasColumnType("longtext CHARACTER SET utf8mb4");
 
                     b.Property<int>("Segmento")
