1162c9f980ca3e1dc2f9041cc28d69c9e8eda985
Bloutware <iurykoz@gmail.com>
2023-06-07 15:01:03 -0300

    refactor(empresas): :recycle: deletando campo cnae
    DELETANDO COLUNA SQL

--- BaseDeProjetos/Controllers/EmpresasController.cs
+++ BaseDeProjetos/Controllers/EmpresasController.cs
@@ -185,7 +185,7 @@ namespace BaseDeProjetos.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Logo,Nome,CNPJ,Segmento,Estado,Industrial,NomeFantasia,CNAE")] Empresa empresa)
+        public async Task<IActionResult> Create([Bind("Id,Logo,Nome,CNPJ,Segmento,Estado,Industrial,NomeFantasia")] Empresa empresa)
         {
             if (ModelState.IsValid)
             {
@@ -229,7 +229,7 @@ namespace BaseDeProjetos.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Logo,Nome,CNPJ,Segmento,Estado,Industrial,NomeFantasia,CNAE")] Empresa empresa)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Logo,Nome,CNPJ,Segmento,Estado,Industrial,NomeFantasia")] Empresa empresa)
         {
             if (id != empresa.Id)
             {
