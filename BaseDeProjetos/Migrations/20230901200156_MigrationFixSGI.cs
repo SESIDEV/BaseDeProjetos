7d77d29dfef7e3976ddb06f71db404d263ffce18
Rafael Magno <rafaelmagno125@yahoo.com.br>
2023-09-01 17:29:59 -0300

    Revert "Revert "Tentar consertar o Migration""
    This reverts commit 0ebb153d4d44fcf885fbd8627151f5b54e81d322.

---
+++ BaseDeProjetos/Migrations/20230901200156_MigrationFixSGI.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseDeProjetos.Migrations
+{
+    public partial class MigrationFixSGI : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "AtividadesProdutivas",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AreaAtividade",
+                table: "AtividadesProdutivas",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Atividade",
+                table: "AtividadesProdutivas",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "CargaHoraria",
+                table: "AtividadesProdutivas",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Data",
+                table: "AtividadesProdutivas",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "DescricaoAtividade",
+                table: "AtividadesProdutivas",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FonteFomento",
+                table: "AtividadesProdutivas",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProjetoId",
+                table: "AtividadesProdutivas",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AtividadesProdutivas",
+                table: "AtividadesProdutivas",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AtividadesProdutivas_UsuarioId",
+                table: "AtividadesProdutivas",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AtividadesProdutivas",
+                table: "AtividadesProdutivas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AtividadesProdutivas_UsuarioId",
+                table: "AtividadesProdutivas");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "AtividadesProdutivas");
+
+            migrationBuilder.DropColumn(
+                name: "AreaAtividade",
+                table: "AtividadesProdutivas");
+
+            migrationBuilder.DropColumn(
+                name: "Atividade",
+                table: "AtividadesProdutivas");
+
+            migrationBuilder.DropColumn(
+                name: "CargaHoraria",
+                table: "AtividadesProdutivas");
+
+            migrationBuilder.DropColumn(
+                name: "Data",
+                table: "AtividadesProdutivas");
+
+            migrationBuilder.DropColumn(
+                name: "DescricaoAtividade",
+                table: "AtividadesProdutivas");
+
+            migrationBuilder.DropColumn(
+                name: "FonteFomento",
+                table: "AtividadesProdutivas");
+
+            migrationBuilder.DropColumn(
+                name: "ProjetoId",
+                table: "AtividadesProdutivas");
+        }
+    }
+}
