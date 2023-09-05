b023af60b7a0e94d21b61d4f92abaec3b817a4ed
Rafael Magno <rafaelmagno125@yahoo.com.br>
2023-09-05 11:14:58 -0300

    iniciando modelos de indicadores de projeto (inclusão de módulo máquina e cargo para calcular HM e HH)

---
+++ BaseDeProjetos/Models/Cargo.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseDeProjetos.Models
+{
+	public class Cargo
+	{
+
+		public int Id { get; set; }
+		private string _nome { get; set; } = "Não informado";
+		private decimal _salario { get; set; }
+		public string Nome
+		{
+			get
+			{
+				return _nome;
+			}
+			set
+			{
+				if (value == "Não informado" || value == null)
+				{
+					_salario = 0;
+				}
+				else
+				{
+					_nome = value;
+				}
+			}
+		}
+		public decimal Salario { get => _salario; set => _salario = value; }
+
+	}
+
+}
