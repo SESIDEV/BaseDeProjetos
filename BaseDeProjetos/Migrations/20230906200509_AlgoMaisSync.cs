d30863d1ab25b46ef69f5491714239bb687c7e76
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-06 17:05:25 -0300

    Consertar migrations para seguir o padrão dos models

---
+++ BaseDeProjetos/Migrations/20230906200509_AlgoMaisSync.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseDeProjetos.Migrations
+{
+    public partial class AlgoMaisSync : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProjetoIndicadores_IdProjeto",
+                table: "ProjetoIndicadores");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjetoIndicadores_IdProjeto",
+                table: "ProjetoIndicadores",
+                column: "IdProjeto",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProjetoIndicadores_IdProjeto",
+                table: "ProjetoIndicadores");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjetoIndicadores_IdProjeto",
+                table: "ProjetoIndicadores",
+                column: "IdProjeto");
+        }
+    }
+}
