dd4da547e184c90b830d53476f9288e0dcab82ba
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-08 16:41:18 -0300

    Adicionar mais uma migration faltando

--- BaseDeProjetos/Migrations/20230906125105_InclusaoDeCargoParaUSuario.cs
+++ BaseDeProjetos/Migrations/20230906125105_InclusaoDeCargoParaUSuario.cs
@@ -6,12 +6,38 @@ namespace BaseDeProjetos.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-           
+            migrationBuilder.AddColumn<int>(
+                name: "CargoId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CargoId",
+                table: "AspNetUsers",
+                column: "CargoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Cargo_CargoId",
+                table: "AspNetUsers",
+                column: "CargoId",
+                principalTable: "Cargo",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Cargo_CargoId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CargoId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CargoId",
+                table: "AspNetUsers");
         }
     }
 }
