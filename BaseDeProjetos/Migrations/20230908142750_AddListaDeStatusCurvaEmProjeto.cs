c4ceffafa05433ad1d9f62431afc30f613900867
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-08 15:57:40 -0300

    Reverter migrations

---
+++ BaseDeProjetos/Migrations/20230908142750_AddListaDeStatusCurvaEmProjeto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseDeProjetos.Migrations
+{
+    public partial class AddListaDeStatusCurvaEmProjeto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
