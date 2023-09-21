d23b6014430803d099400d712b8a9995c8e38ef6
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-21 13:24:32 -0300

    Criar migration para Cargo (tabela)

---
+++ BaseDeProjetos/Migrations/20230921162410_Cargo.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseDeProjetos.Migrations
+{
+    public partial class Cargo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comentarios",
+                table: "PesquisaProjeto",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Cargo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Salario = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cargo", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Cargo_CargoId",
+                table: "AspNetUsers",
+                column: "CargoId",
+                principalTable: "Cargo",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Cargo_CargoId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Cargo");
+
+            migrationBuilder.DropColumn(
+                name: "Comentarios",
+                table: "PesquisaProjeto");
+        }
+    }
+}
