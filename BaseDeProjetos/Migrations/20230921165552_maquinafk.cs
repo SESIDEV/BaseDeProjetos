97ebc3805613ad99aa82515ab8cc97bb7d0ade3a
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-21 13:56:38 -0300

    Adicionar migration para MaquinaFK
    Agora sim. Meu jesus.

---
+++ BaseDeProjetos/Migrations/20230921165552_maquinafk.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseDeProjetos.Migrations
+{
+    public partial class maquinafk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Adicionado manualmente favor não remover
+            migrationBuilder.AddColumn<int>(
+                name: "ProjetoId",
+                table: "Maquina",
+                nullable: true
+            );
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maquina_ProjetoId",
+                table: "Maquina",
+                column: "ProjetoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Maquina_ProjetoId",
+                table: "Maquina");
+
+            migrationBuilder.DropColumn(
+                name: "ProjetoId",
+                table: "Maquina");
+
+        }
+    }
+}
