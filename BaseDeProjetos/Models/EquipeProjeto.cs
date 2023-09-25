68b803ce515236d2c323badf52f90b8f84f23c50
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-25 17:17:28 -0300

    Criar modelo equipe projeto

---
+++ BaseDeProjetos/Models/EquipeProjeto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BaseDeProjetos.Models
+{
+    public class EquipeProjeto : IEquipe
+    {
+        public int Id { get; set; }
+        public string IdTrabalho { get; set; }
+
+        [ForeignKey("IdTrabalho")]
+        public virtual Projeto Projeto { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+        public string IdUsuario { get; set; }
+    }
+}
