9704c4c2aedd4bde2e9c378fda556aa4a2d9944b
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-25 17:17:36 -0300

    Criar interface para o modelo equipeprojeto

---
+++ BaseDeProjetos/Models/IEquipe.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BaseDeProjetos.Models
+{
+    public interface IEquipe
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string IdTrabalho { get; set; }
+
+        public Usuario Usuario { get; set; }
+        public string IdUsuario { get; set; }
+    }
+}
