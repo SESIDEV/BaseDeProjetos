4e402cc37f7bc5cfb77f50b4f5afc75a2f950023
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-26 16:30:47 -0300

    Adicionar EquipeProjeto ao DbContext

--- BaseDeProjetos/Data/ApplicationDbContext.cs
+++ BaseDeProjetos/Data/ApplicationDbContext.cs
@@ -10,23 +10,22 @@ namespace BaseDeProjetos.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-
-
         }
 
-        public DbSet<BaseDeProjetos.Models.StatusCurva> StatusCurva { get; set; }
-        public DbSet<BaseDeProjetos.Models.Projeto> Projeto { get; set; }
-        public DbSet<BaseDeProjetos.Models.Prospeccao> Prospeccao { get; set; }
-        public DbSet<BaseDeProjetos.Models.FollowUp> FollowUp { get; set; }
-        public DbSet<BaseDeProjetos.Models.Empresa> Empresa { get; set; }
-        public DbSet<BaseDeProjetos.Models.Pessoa> Pessoa { get; set; }
-        public DbSet<BaseDeProjetos.Models.AtividadesProdutivas> AtividadesProdutivas { get; set; }
-        public DbSet<BaseDeProjetos.Models.IndicadoresFinanceiros> IndicadoresFinanceiros { get; set; }
-        public DbSet<BaseDeProjetos.Models.Producao> Producao { get; set; }
-        public DbSet<BaseDeProjetos.Models.Editais> Editais { get; set; }
-        public DbSet<BaseDeProjetos.Models.Submissao> Submissao { get; set; }
-        public DbSet<BaseDeProjetos.Models.ProjetoIndicadores> ProjetoIndicadores { get; set; }
-		public DbSet<BaseDeProjetos.Models.Cargo> Cargo { get; set; }
+        public DbSet<StatusCurva> StatusCurva { get; set; }
+        public DbSet<Projeto> Projeto { get; set; }
+        public DbSet<Prospeccao> Prospeccao { get; set; }
+        public DbSet<FollowUp> FollowUp { get; set; }
+        public DbSet<Empresa> Empresa { get; set; }
+        public DbSet<Pessoa> Pessoa { get; set; }
+        public DbSet<AtividadesProdutivas> AtividadesProdutivas { get; set; }
+        public DbSet<IndicadoresFinanceiros> IndicadoresFinanceiros { get; set; }
+        public DbSet<Producao> Producao { get; set; }
+        public DbSet<Editais> Editais { get; set; }
+        public DbSet<Submissao> Submissao { get; set; }
+        public DbSet<ProjetoIndicadores> ProjetoIndicadores { get; set; }
+        public DbSet<Cargo> Cargo { get; set; }
         public DbSet<PesquisaProjeto> PesquisaProjeto { get; set; }
-	}
+        public DbSet<EquipeProjeto> EquipeProjeto { get; set; }
+    }
 }
