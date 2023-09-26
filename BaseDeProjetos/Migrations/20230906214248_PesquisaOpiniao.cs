21d0d5853b45fdd0d2eb2776fafd57778e9e1dc1
hhenriques1999 <hhenriques1999@gmail.com>
2023-09-26 10:37:26 -0300

    Revert "Fixup migration"
    This reverts commit ccd5e5f3788d874688fd4eb5b1eca33a9cdd0d75.

--- BaseDeProjetos/Migrations/20230906214248_PesquisaOpiniao.cs
+++ BaseDeProjetos/Migrations/20230906214248_PesquisaOpiniao.cs
@@ -3,34 +3,51 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace BaseDeProjetos.Migrations
 {
-	public partial class PesquisaOpiniao : Migration
-	{
-		protected override void Up(MigrationBuilder migrationBuilder)
-		{
-			migrationBuilder.DropColumn(
-				name: "ValorSatisfacaoFimProjeto",
-				table: "ProjetoIndicadores");
+    public partial class PesquisaOpiniao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValorSatisfacaoFimProjeto",
+                table: "ProjetoIndicadores");
 
-			migrationBuilder.DropColumn(
-				name: "ValorSatisfacaoMetadeProjeto",
-				table: "ProjetoIndicadores");
-		}
+            migrationBuilder.DropColumn(
+                name: "ValorSatisfacaoMetadeProjeto",
+                table: "ProjetoIndicadores");
 
-		protected override void Down(MigrationBuilder migrationBuilder)
-		{
-			migrationBuilder.AddColumn<int>(
-				name: "ValorSatisfacaoFimProjeto",
-				table: "ProjetoIndicadores",
-				type: "int",
-				nullable: false,
-				defaultValue: 0);
+            migrationBuilder.CreateTable(
+                name: "PesquisaProjeto",
+                columns: table => new
+                {
+                    IdPesquisa = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProjetoId = table.Column<string>(nullable: true),
+                    ResultadoFinal = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PesquisaProjeto", x => x.IdPesquisa);
+                });
+        }
 
-			migrationBuilder.AddColumn<int>(
-				name: "ValorSatisfacaoMetadeProjeto",
-				table: "ProjetoIndicadores",
-				type: "int",
-				nullable: false,
-				defaultValue: 0);
-		}
-	}
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PesquisaProjeto");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ValorSatisfacaoFimProjeto",
+                table: "ProjetoIndicadores",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ValorSatisfacaoMetadeProjeto",
+                table: "ProjetoIndicadores",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
 }
