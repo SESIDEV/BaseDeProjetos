459ecf1b2e73f5c1d7bfcad3604ac39a617c5d44
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-02 16:14:22 -0300

    Criar classe helper para criação de objetos de testagem em ProjetosController

---
+++ TestesBaseDeProjetos1/TestHelper/ObjectCreator.cs
@@ -0,0 +1,84 @@
+﻿using BaseDeProjetos.Models;
+using NUnit.Framework.Constraints;
+using System;
+
+namespace TestesBaseDeProjetos1.TestHelper
+{
+
+    internal class ObjectCreator
+    {
+        public ProjetoIndicadores projetoIndicadores;
+        public Usuario usuario;
+        public Empresa empresa;
+        public Cargo cargo;
+        public Projeto projeto;
+
+        public void CriarEmpresaMock()
+        {
+            empresa = new Empresa()
+            {
+                Id = 1,
+                Nome = "Minha Empresa",
+                RazaoSocial = "Laticineos Ultimate, Inc",
+                CNPJ = "12345678909876",
+                Estado = Estado.Rondonia,
+            };
+        }
+
+        public Cargo CriarCargoMock()
+        {
+            return new Cargo() { Id = 1, Nome = "Pesquisador", Salario = 7500 };
+        }
+
+        public void CriarProjetoIndicadoresMock()
+        {
+            projetoIndicadores = new ProjetoIndicadores()
+            {
+                Id = "indicador_1",
+                Bolsista = true
+            };
+        }
+
+        public void CriarUsuarioMock()
+        {
+            usuario = new Usuario()
+            {
+                Id = "myUser",
+                Cargo = cargo,
+                Casa = Instituto.ISIQV,
+                CargoId = 1,
+                Competencia = "Alguma",
+                Email = "meuemail@firjan.com.br",
+                Foto = "",
+                Matricula = 12312,
+                Nivel = Nivel.Usuario,
+                Vinculo = TipoVinculo.Empregado,
+                EmailConfirmed = true,
+                Titulacao = Titulacao.Graduado,
+                UserName = "username", // Deve bater com o mock dos claims
+            };
+        }
+
+        public void CriarProjetoMock()
+        {
+            projeto = new Projeto(projetoIndicadores)
+            {
+                Id = "proj_1",
+                AreaPesquisa = LinhaPesquisa.Quimica40,
+                Casa = Instituto.ISIQV,
+                DataInicio = new DateTime(2013, 09, 23),
+                DataEncerramento = new DateTime(2023, 09, 23),
+                SatisfacaoCliente = 0.97f,
+                NomeProjeto = "Meu Projeto Teste",
+                Empresa = empresa,
+                Estado = Estado.RioDeJaneiro,
+                Inovacao = TipoInovacao.Processo,
+                Status = StatusProjeto.Contratado,
+                DuracaoProjetoEmMeses = 240,
+                ValorTotalProjeto = 123000,
+                Usuario = usuario,
+                UsuarioId = usuario.Id,
+            };
+        }
+    }
+}
