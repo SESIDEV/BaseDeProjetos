64754155cd2b0aa593e653b80955cd3480794018
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-03 14:56:13 -0300

    Adicionar migration de Satisfacao Parcial e Satisfacao Final

---
+++ BaseDeProjetos/Migrations/20231003164112_SatisfacaoParcialFinal.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseDeProjetos.Migrations
+{
+    public partial class SatisfacaoParcialFinal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SatisfacaoCliente",
+                table: "Projeto");
+
+            migrationBuilder.AddColumn<float>(
+                name: "SatisfacaoClienteFinal",
+                table: "Projeto",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "SatisfacaoClienteParcial",
+                table: "Projeto",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SatisfacaoClienteFinal",
+                table: "Projeto");
+
+            migrationBuilder.DropColumn(
+                name: "SatisfacaoClienteParcial",
+                table: "Projeto");
+
+            migrationBuilder.AddColumn<float>(
+                name: "SatisfacaoCliente",
+                table: "Projeto",
+                type: "float",
+                nullable: true);
+        }
+    }
+}
