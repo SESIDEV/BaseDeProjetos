2b1c637d09f229163e248cec36f9c14b1be892e6
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-03 16:33:43 -0300

    Migration para horas semanais em cargo

---
+++ BaseDeProjetos/Migrations/20231003192237_HorasSemanaisCargo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseDeProjetos.Migrations
+{
+    public partial class HorasSemanaisCargo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HorasSemanais",
+                table: "Cargo",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HorasSemanais",
+                table: "Cargo");
+        }
+    }
+}
