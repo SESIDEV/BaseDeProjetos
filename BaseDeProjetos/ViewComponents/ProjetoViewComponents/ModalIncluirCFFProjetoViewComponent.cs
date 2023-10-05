fc05bc6529a3abfe8691ce2d081fed1287fab1cb
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-05 17:28:34 -0300

    Criar ViewComponents para os modais de CFF

---
+++ BaseDeProjetos/ViewComponents/ProjetoViewComponents/ModalIncluirCFFProjetoViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using BaseDeProjetos.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BaseDeProjetos.ViewComponents.ProjetoViewComponents
+{
+    public class ModalIncluirCFFProjetoViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ModalIncluirCFFProjetoViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+            ViewData["Projeto"] = await _context.Projeto.FindAsync(id);
+            return View();
+        }
+    }
+}
