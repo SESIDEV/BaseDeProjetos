8ad2f1dbe7b9cba124d19c1a5f20f0dde059e1ca
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-05 16:56:56 -0300

    Adicionar migration para CFF

---
+++ BaseDeProjetos/Migrations/20231005195549_cff.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseDeProjetos.Migrations
+{
+    public partial class cff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Financeiro",
+                table: "StatusCurva");
+
+            migrationBuilder.DropColumn(
+                name: "Fisico",
+                table: "StatusCurva");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PercentualFinanceiro",
+                table: "StatusCurva",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PercentualFisico",
+                table: "StatusCurva",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PercentualFinanceiro",
+                table: "StatusCurva");
+
+            migrationBuilder.DropColumn(
+                name: "PercentualFisico",
+                table: "StatusCurva");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Financeiro",
+                table: "StatusCurva",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Fisico",
+                table: "StatusCurva",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
