6bcbcfcfcf333c2879d4398c043cf0f98d9e2a34
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-09 15:22:40 -0300

    Incluir diretiva de assembly de visibilidade para os testes

--- BaseDeProjetos/Controllers/ParticipacaoController.cs
+++ BaseDeProjetos/Controllers/ParticipacaoController.cs
@@ -9,7 +9,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("TestesBaseDeProjetos1")]
 
 namespace BaseDeProjetos.Controllers
 {
