e3d23692da83b3dcb6b011589dfbbd47725b99f6
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-09 15:21:47 -0300

    Renomear classe de testes de despesa para separar os testes apenas para a despesa

--- TestesBaseDeProjetos1/Controllers/ParticipacaoController_CalculoDespesaTests.cs
+++ TestesBaseDeProjetos1/Controllers/ParticipacaoController_CalculoDespesaTests.cs
@@ -1,9 +1,10 @@
 ﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace BaseDeProjetos.Controllers.Tests
 {
-    public class ParticipacaoControllerTests
+    public class ParticipacaoController_CalculoDespesaTests
     {
         // Assuming this dictionary represents the annual expenses for certain years
         private Dictionary<int, decimal> despesas = new Dictionary<int, decimal>
@@ -17,21 +18,21 @@ namespace BaseDeProjetos.Controllers.Tests
         public void Test_MesmoAnoMesmoMes()
         {
             var result = ParticipacaoController.CalculoDespesa(01, 2021, 01, 2021);
-            Assert.AreEqual(290000M / 12, result);
+            Assert.AreEqual(despesas[2021] / 12, result);
         }
 
         [Test]
-        public void Test_MesmoAnoMesDiferente() // TODO: Verificar
+        public void Test_MesmoAnoMesDiferente()
         {
             var result = ParticipacaoController.CalculoDespesa(01, 2021, 06, 2021);
-            Assert.AreEqual(290000M / 2, result); // Assuming the monthly expense for 2021 is 100 (1200/12)
+            Assert.AreEqual(despesas[2021] / 12 * 6, result);
         }
 
         [Test]
         public void Test_AnoDiferenteMesmoMes()
         {
             var result = ParticipacaoController.CalculoDespesa(01, 2021, 01, 2022);
-            Assert.AreEqual(290000M + (400000M / 12), result);
+            Assert.AreEqual(despesas[2021] + (despesas[2022] / 12), result);
         }
 
         [Test]
@@ -45,20 +46,25 @@ namespace BaseDeProjetos.Controllers.Tests
         public void Test_MultiplosAnosDiferentesMesIgual()
         {
             var result = ParticipacaoController.CalculoDespesa(06, 2021, 06, 2023);
-            Assert.AreEqual(despesas[2021] / 12 * 7 + despesas[2022] + despesas[2023] / 2, result);
+            Assert.AreEqual((despesas[2021] / 12 * 7) + despesas[2022] + despesas[2023] / 2, result);
         }
 
         [Test]
         public void Test_MultiplosAnosDiferentesMesesDiferentes()
         {
             var result = ParticipacaoController.CalculoDespesa(01, 2021, 09, 2023);
-            Assert.AreEqual(despesas[2021] + despesas[2022] + despesas[2023] / 12 * 9, result);
+            Assert.AreEqual(despesas[2021] + despesas[2022] + (despesas[2023] / 12 * 9), result);
         }
 
         [Test]
         public void Test_AnoNaoExistente()
         {
-            Assert.Throws<KeyNotFoundException>(() => ParticipacaoController.CalculoDespesa(01, 2020, 01, 2021)); // 2020 is not in the dictionary
+            Assert.Throws<KeyNotFoundException>(() => ParticipacaoController.CalculoDespesa(01, 2020, 01, 2021));
+        }
+
+        public void Test_AnoInicialMaiorQueFinal()
+        {
+            Assert.Throws<ArgumentException>(() => ParticipacaoController.CalculoDespesa(01, 2022, 01, 2023));
         }
     }
 }
