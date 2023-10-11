97ee5d67c488eac9280f7cd55352e31dd26d212c
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-11 14:23:25 -0300

    Adaptar CalculoNumeroPesquisadoresTests ao formato padrão

--- TestesBaseDeProjetos1/Controllers/ParticipacaoController/CalculoNumeroPesquisadoresTests.cs
+++ TestesBaseDeProjetos1/Controllers/ParticipacaoController/CalculoNumeroPesquisadoresTests.cs
@@ -1,13 +1,26 @@
 ﻿using BaseDeProjetos.Controllers;
+using BaseDeProjetos.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text;
+using TestesBaseDeProjetos1.TestHelper;
 
 namespace BaseDeProjetos.Controllers.Tests.ParticipacaoControllerTests
 {
+
+    [TestFixture]
     internal class CalculoNumeroPesquisadoresTests
     {
+        private ApplicationDbContext _context;
+        private ObjectCreator _objectCreator;
+        private ParticipacaoController _controller;
+
         private static Dictionary<int, int> pesquisadores = new Dictionary<int, int>
         {
             {2021, 20},
@@ -15,23 +28,62 @@ namespace BaseDeProjetos.Controllers.Tests.ParticipacaoControllerTests
             {2023, 23}
         };
 
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) // Separar o contexto do banco para cada teste
+                .Options;
+
+            _context = new ApplicationDbContext(options);
+            _context.Database.EnsureCreated();
+
+            _objectCreator = new ObjectCreator();
+
+            // Não mude a ordem!!
+            _objectCreator.CriarProjetoIndicadoresMock();
+            _objectCreator.CriarCargoMock();
+            _objectCreator.CriarUsuarioMock();
+            _objectCreator.CriarEmpresaMock();
+            _objectCreator.CriarProjetoMock();
+
+            _controller = new ParticipacaoController(_context);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "username"),
+            }));
+
+            var sessionMock = new Mock<ISession>();
+
+            var httpContextMock = new Mock<HttpContext>();
+
+            httpContextMock.Setup(x => x.User).Returns(user);
+            httpContextMock.Setup(y => y.Session).Returns(sessionMock.Object);
+
+            _controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = httpContextMock.Object,
+            };
+        }
+
         [Test]
         public void Test_ThrowsException_AnoInicialMaiorQueAnoFinal()
         {
-            Assert.Throws<ArgumentException>(() => ParticipacaoController.CalculoNumeroPesquisadores(2020, 2019));
+            Assert.Throws<ArgumentException>(() => _controller.CalculoNumeroPesquisadores(2020, 2019));
         }
 
         [Test]
         public void Test_ReturnsPesquisadoresForAnoFinal_WhenAnoInicialEqualsAnoFinal()
         {
-            var result = ParticipacaoController.CalculoNumeroPesquisadores(2021, 2021);
+            var result = _controller.CalculoNumeroPesquisadores(2021, 2021);
             Assert.AreEqual(pesquisadores[2021], result);
         }
 
         [Test]
         public void Test_ReturnsSumOfPesquisadoresInRange_QuandoAnoInicialMenorQueAnoFinal()
         {
-            var result = ParticipacaoController.CalculoNumeroPesquisadores(2021, 2022);
+            var result = _controller.CalculoNumeroPesquisadores(2021, 2022);
             Assert.AreEqual((pesquisadores[2021] + pesquisadores[2022]) / 2, result);
         }
     }
