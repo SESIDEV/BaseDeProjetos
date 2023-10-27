4fa62afa2805be1264e491f50c3e65b945051ba8
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-27 13:51:59 -0300

    Criar rota escondida para criar usuários a partir de uma lista pré-definida

--- BaseDeProjetos/Controllers/EditaisController.cs
+++ BaseDeProjetos/Controllers/EditaisController.cs
@@ -1,6 +1,7 @@
 ﻿using BaseDeProjetos.Data;
 using BaseDeProjetos.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -16,8 +17,79 @@ namespace BaseDeProjetos.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public EditaisController(ApplicationDbContext context)
+        private readonly UserManager<Usuario> _userManager;
+
+        [HttpGet]
+        public async Task<IActionResult> CreateUsers()
         {
+            ViewbagizarUsuario(_context);
+
+            if (HttpContext.User.Identity.IsAuthenticated && UsuarioAtivo.Nivel == Nivel.Dev) // Apenas devs
+            {
+                await AddMultipleUsers(); // The function from the previous answer
+                return View(); // Redirect to a confirmation page or wherever you want after creating users.
+            }
+            else
+            {
+                return View("Forbidden");
+            }
+        }
+
+        public async Task AddMultipleUsers()
+        {
+
+            var users = new List<Usuario>
+            {
+                // Cadastro bolsistas
+                new Usuario { UserName = "Beatriz Guimarães", Email = "bsguimaraes@pesquisador.firjan.com.br", Casa = Instituto.ISIQV, CargoId = 2, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Débora Oliveira", Email = "drdoliveira@pesquisador.firjan.com.br", Casa = Instituto.ISIQV, CargoId = 2, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Bruna Borges", Email = "bbasilva@pesquisador.firjan.com.br", Casa = Instituto.ISIQV, CargoId = 2, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Andreza Mendonça", Email = "admendonca@pesquisador.firjan.com.br", Casa = Instituto.ISIQV, CargoId = 2, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Thuanny Moraes", Email = "tmoraes@pesquisador.firjan.com.br", Casa = Instituto.ISIQV, CargoId = 2, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Geovanna Batista", Email = "gbflorentino@pesquisador.firjan.com.br", Casa = Instituto.ISIQV, CargoId = 2, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Carlos Magno", Email = "carlosms@pesquisador.firjan.com.br", Casa = Instituto.ISIQV, CargoId = 2, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+         
+                // Cadastro estagiários
+                new Usuario { UserName = "Gabriel Mattos", Email = "gserra@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0, EmailConfirmed = false },
+                new Usuario { UserName = "Caio Vitor", Email = "ccadete@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0, EmailConfirmed = false },
+                new Usuario { UserName = "Caio de Souza", Email = "cdmoreira@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Mariana de Paula", Email = "mdbatista@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Lucas Rocha", Email = "lucrocha@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Maria Carolina", Email = "mcmarques@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Juliana Peres", Email = "jpborges@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Pedro Tagliabui", Email = "ppduarte@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                
+                // Cadastro de usuários "dummy" (idealmente deveremos encontrar uma forma mais dinâmica de fazer isso no futuro, mas pela "correria", vai desse jeito.
+                new Usuario { UserName = "Estagiário 1", Email = "estag1@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Estagiário 2", Email = "estag2@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Estagiário 3", Email = "estag3@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Estagiário 4", Email = "estag4@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Estagiário 5", Email = "estag5@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 3, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Bolsista 1", Email = "bols1@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 2, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Bolsista 2", Email = "bols2@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 2, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Bolsista 3", Email = "bols3@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 2, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Bolsista 4", Email = "bols4@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 2, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+                new Usuario { UserName = "Bolsista 5", Email = "bols5@firjan.com.br", Casa = Instituto.ISIQV, CargoId = 2, Matricula = 0, Nivel = 0, Vinculo = 0 , EmailConfirmed = false},
+            };
+
+            foreach (var user in users)
+            {
+                var result = await _userManager.CreateAsync(user, "@ISIqv1234"); // Replace 'DefaultPassword' with your desired password.
+                if (!result.Succeeded)
+                {
+                    // Handle error - for instance, the user might already exist, or there might be password validation errors.
+                    foreach (var error in result.Errors)
+                    {
+                        // Log or display the error description.
+                        Console.WriteLine(error.Description);
+                    }
+                }
+            }
+        }
+
+        public EditaisController(ApplicationDbContext context, UserManager<Usuario> userManager)
+        {
+            _userManager = userManager;
             _context = context;
         }
 
