5655798e5cbe2082b9700dce00123a67614347b7
hhenriques1999 <hhenriques1999@gmail.com>
2023-11-06 16:41:42 -0300

    Atualizar DTO empresas

--- BaseDeProjetos/Models/EmpresasDTOs.cs
+++ BaseDeProjetos/Models/EmpresasDTOs.cs
@@ -8,6 +8,9 @@ namespace BaseDeProjetos.Models
     {
         public string Id { get; set; }
         public virtual List<FollowUp> Status { get; set; }
+        public virtual Empresa Empresa { get; set; }
+        public virtual string NomeProspeccao { get; set; }
+        public virtual Usuario Usuario { get; set; }
     }
 
     public class EmpresaDTO
