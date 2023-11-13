32614465be07547fdd3b96040e48b41624d8325b
hhenriques1999 <hhenriques1999@gmail.com>
2023-11-13 14:55:07 -0300

    Ajustar testes e consertar typo

--- TestesBaseDeProjetos1/Controllers/ParticipacaoController/CalculoDespesaTests.cs
+++ TestesBaseDeProjetos1/Controllers/ParticipacaoController/CalculoDespesaTests.cs
@@ -1,12 +1,16 @@
-﻿using NUnit.Framework;
+﻿using BaseDeProjetos.Data;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BaseDeProjetos.Controllers.Tests.ParticipacaoControllerTests
 {
     public class CalculoDespesaTests
     {
-        // Assuming this dictionary represents the annual expenses for certain years
+        private ApplicationDbContext? _context;
+
         private readonly Dictionary<int, decimal> despesas = new Dictionary<int, decimal>
         {
             {2021, 290000},
@@ -14,57 +18,71 @@ namespace BaseDeProjetos.Controllers.Tests.ParticipacaoControllerTests
             {2023, 440000}
         };
 
-        [Test]
-        public void Test_MesmoAnoMesmoMes()
+        [SetUp]
+        public void Setup()
         {
-            var result = ParticipacaoController.CalculoDespesa(01, 2021, 01, 2021);
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) // Separar o contexto do banco para cada teste
+                .Options;
+
+            if (_context == null)
+            {
+                _context = new ApplicationDbContext(options);
+                _context.Database.EnsureCreated();
+            }
+        }
+
+        [Test]
+        public async Task Test_MesmoAnoMesmoMes()
+        {
+            var result = await ParticipacaoController.CalculoDespesa(_context, 01, 2021, 01, 2021);
             Assert.AreEqual(despesas[2021] / 12, result);
         }
 
         [Test]
-        public void Test_MesmoAnoMesDiferente()
+        public async Task Test_MesmoAnoMesDiferente()
         {
-            var result = ParticipacaoController.CalculoDespesa(01, 2021, 06, 2021);
+            var result = await ParticipacaoController.CalculoDespesa(_context, 01, 2021, 06, 2021);
             Assert.AreEqual(despesas[2021] / 12 * 6, result);
         }
 
         [Test]
-        public void Test_AnoDiferenteMesmoMes()
+        public async Task Test_AnoDiferenteMesmoMes()
         {
-            var result = ParticipacaoController.CalculoDespesa(01, 2021, 01, 2022);
+            var result = await ParticipacaoController.CalculoDespesa(_context, 01, 2021, 01, 2022);
             Assert.AreEqual(despesas[2021] + (despesas[2022] / 12), result);
         }
 
         [Test]
-        public void Test_AnoDiferenteMesDiferente()
+        public async Task Test_AnoDiferenteMesDiferente()
         {
-            var result = ParticipacaoController.CalculoDespesa(01, 2021, 06, 2022);
+            var result = await ParticipacaoController.CalculoDespesa(_context, 01, 2021, 06, 2022);
             Assert.AreEqual(despesas[2021] + despesas[2022] / 2, result);
         }
 
         [Test]
-        public void Test_MultiplosAnosDiferentesMesIgual()
+        public async Task Test_MultiplosAnosDiferentesMesIgual()
         {
-            var result = ParticipacaoController.CalculoDespesa(06, 2021, 06, 2023);
+            var result = await ParticipacaoController.CalculoDespesa(_context, 06, 2021, 06, 2023);
             Assert.AreEqual((despesas[2021] / 12 * 7) + despesas[2022] + despesas[2023] / 2, result);
         }
 
         [Test]
-        public void Test_MultiplosAnosDiferentesMesesDiferentes()
+        public async Task Test_MultiplosAnosDiferentesMesesDiferentes()
         {
-            var result = ParticipacaoController.CalculoDespesa(01, 2021, 09, 2023);
+            var result = await ParticipacaoController.CalculoDespesa(_context, 01, 2021, 09, 2023);
             Assert.AreEqual(despesas[2021] + despesas[2022] + (despesas[2023] / 12 * 9), result);
         }
 
         [Test]
         public void Test_AnoNaoExistente()
         {
-            Assert.Throws<KeyNotFoundException>(() => ParticipacaoController.CalculoDespesa(01, 2020, 01, 2021));
+            Assert.Throws<KeyNotFoundException>(() => ParticipacaoController.CalculoDespesa(_context, 01, 2020, 01, 2021).GetAwaiter().GetResult());
         }
 
         public void Test_AnoInicialMaiorQueFinal()
         {
-            Assert.Throws<ArgumentException>(() => ParticipacaoController.CalculoDespesa(01, 2022, 01, 2023));
+            Assert.Throws<ArgumentException>(() => ParticipacaoController.CalculoDespesa(_context, 01, 2022, 01, 2023).GetAwaiter().GetResult());
         }
     }
 }
