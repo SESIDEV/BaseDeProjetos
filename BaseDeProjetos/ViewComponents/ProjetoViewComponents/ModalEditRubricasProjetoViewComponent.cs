ac39216932cbc1a0b630c5aaefac81dfe90e92d7
hhenriques1999 <hhenriques1999@gmail.com>
2023-11-16 17:37:18 -0300

    Criar modal de editar rubricas

---
+++ BaseDeProjetos/ViewComponents/ProjetoViewComponents/ModalEditRubricasProjetoViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using BaseDeProjetos.Data;
+using BaseDeProjetos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BaseDeProjetos.ViewComponents.ProjetoViewComponents
+{
+    public class ModalEditRubricasProjetoViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ModalEditRubricasProjetoViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var model = await _context.Rubrica.FindAsync(id);
+            return View(model);
+        }
+    }
+}
