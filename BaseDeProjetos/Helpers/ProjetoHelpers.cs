7d7d16865e3492aaaa959092e61622f5561f8733
hhenriques1999 <hhenriques1999@gmail.com>
2023-11-16 14:49:48 -0300

    Criar helpers para spawnar os containers de modais

---
+++ BaseDeProjetos/Helpers/ProjetoHelpers.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Html;
+using System.Text;
+
+namespace BaseDeProjetos.Helpers
+{
+    public static class ProjetoHelpers
+    {
+        public static HtmlString GerarContainersModais(string projetoId)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] modalTypes = new string[]
+            {
+                "Edit", "Delete", "History", "Details",
+                "IncluirIndicador", "DetalhesIndicador", "IncluirCFF",
+                "DetalhesCFF", "EditCFF", "DetalhesRubricas", "IncluirRubricas"
+            };
+
+            foreach (var modalType in modalTypes)
+            {
+                sb.Append($"<div id=\"modal{modalType}Projeto-{projetoId}-container\"></div>\n");
+            }
+
+            return new HtmlString(sb.ToString());
+        }
+    }
+}
