85ea753c2bd4f35f206f3711fd54af6470777d80
hhenriques1999 <hhenriques1999@gmail.com>
2023-11-17 15:46:48 -0300

    Formatar modais.js e consertar verificação de nulidade

--- BaseDeProjetos/wwwroot/js/modais.js
+++ BaseDeProjetos/wwwroot/js/modais.js
@@ -1,33 +1,39 @@
 let modalCarregando = false;
 
 function carregarModalCFF(event, idCFF) {
-  if (modalCarregando) {
-    return; // Exit function if it's already executing
-  }
-  
-  modalCarregando = true; // Set the flag to true to indicate the function is executing
-  
-  let sourceElement = event.target;
-  let modalCFFcontainer = document.querySelector(`#modalEditCFFProjeto-${idCFF}-container`);
+    if (modalCarregando) {
+        return; // Exit function if it's already executing
+    }
 
-  if (modalCFFcontainer.innerHTML && modalCFFcontainer.innerHTML.trim() === '') {
-    sourceElement.disabled = true;
-    let previousInner = sourceElement.innerHTML;
-    sourceElement.innerHTML = '<div class="spinner-border text-light"></div>';
-    fetch(`/Projetos/RetornarModalEditCFF?idCFF=${idCFF}`)
-      .then(response => response.text())
-      .then(result => {
-        sourceElement.innerHTML = previousInner;
-        modalCFFcontainer.innerHTML = result;
-        sourceElement.disabled = false;
-        console.log(modalCFFcontainer);
-      })
-      .finally(() => {
-        modalCarregando = false; // Reset the flag to false after function has executed
-      });
-  } else {
-    modalCarregando = false; // Reset the flag if no fetch call is made
-  }
+    modalCarregando = true; // Set the flag to true to indicate the function is executing
+
+    let sourceElement = event.target;
+    let modalCFFcontainer = document.querySelector(`#modalEditCFFProjeto-${idCFF}-container`);
+
+    if (modalCFFcontainer != null) {
+        if ((modalCFFcontainer.innerHTML != null || modalCFFcontainer.innerHTML != undefined) && modalCFFcontainer.innerHTML.trim() === '') {
+            sourceElement.disabled = true;
+            let previousInner = sourceElement.innerHTML;
+            sourceElement.innerHTML = '<div class="spinner-border text-light"></div>';
+            fetch(`/Projetos/RetornarModalEditCFF?idCFF=${idCFF}`)
+                .then(response => response.text())
+                .then(result => {
+                    sourceElement.innerHTML = previousInner;
+                    modalCFFcontainer.innerHTML = result;
+                    sourceElement.disabled = false;
+                    console.log(modalCFFcontainer);
+                })
+                .finally(() => {
+                    modalCarregando = false; // Reset the flag to false after function has executed
+                });
+        } else {
+            modalCarregando = false; // Reset the flag if no fetch call is made
+        }
+    }
+    else {
+        console.error("ModalCFFContainer is null for idCFF: ", idCFF);
+    }
+
 }
 
 function carregarModalRubrica(event, idRubrica) {
@@ -41,7 +47,6 @@ function carregarModalRubrica(event, idRubrica) {
     let modalRubricasContainer = document.querySelector(`#modalEditRubricasProjeto-${idRubrica}-container`);
 
     if ((modalRubricasContainer.innerHTML != null || modalRubricasContainer.innerHTML != undefined) && modalRubricasContainer.innerHTML.trim() === '') {
-        console.log("Trimmed. Loading");
         sourceElement.disabled = true;
         let previousInner = sourceElement.innerHTML;
         sourceElement.innerHTML = '<div class="spinner-border text-light"></div>';
@@ -57,8 +62,6 @@ function carregarModalRubrica(event, idRubrica) {
                 modalCarregando = false; // Reset the flag to false after function has executed
             });
     } else {
-        console.log("Not trimmed?");
-
         modalCarregando = false; // Reset the flag if no fetch call is made
     }
 }
