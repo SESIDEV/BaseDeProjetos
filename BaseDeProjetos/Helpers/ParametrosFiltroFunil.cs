944b0663cca7e4c0eed83d80645970d749c5fd0d
hhenriques1999 <hhenriques1999@gmail.com>
2023-12-14 16:11:58 -0300

    Criar classe auxiliar para os parâmetros do funil

---
+++ BaseDeProjetos/Helpers/ParametrosFiltroFunil.cs
@@ -0,0 +1,24 @@
+﻿using BaseDeProjetos.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace BaseDeProjetos.Helpers
+{
+    public class ParametrosFiltroFunil
+    {
+        public string Casa { get; set; }
+        public Usuario Usuario { get; set; }
+        public HttpContext HttpContext { get; set; }
+
+        public DbCache Cache { get; set; }
+        public string Aba { get; set; }
+
+        public ParametrosFiltroFunil(string casa, Usuario usuario, DbCache cache, string aba, HttpContext httpContext)
+        {
+            Casa = casa;
+            Usuario = usuario;
+            Cache = cache;
+            Aba = aba;
+            HttpContext = httpContext;
+        }
+    }
+}
