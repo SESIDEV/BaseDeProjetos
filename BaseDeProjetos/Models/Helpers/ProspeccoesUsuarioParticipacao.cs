84aca6fbdc5c2c49fe155da35e4adccb730dd437
hhenriques1999 <hhenriques1999@gmail.com>
2024-01-04 08:47:57 -0300

    Criar classe helper para guardar os dados de prospeccoes do usuario para participacao

---
+++ BaseDeProjetos/Models/Helpers/ProspeccoesUsuarioParticipacao.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BaseDeProjetos.Models.Helpers
+{
+    public class ProspeccoesUsuarioParticipacao
+    {
+        public List<Prospeccao> ProspeccoesTotais { get; set; }
+        public List<Prospeccao> ProspeccoesLider { get; set; }
+        public List<Prospeccao> ProspeccoesMembro { get; set; }
+    }
+}
