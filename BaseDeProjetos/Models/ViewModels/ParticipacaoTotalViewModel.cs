322f5f8fd8bdebd4ab74cd4023193ad23064e0e6
hhenriques1999 <hhenriques1999@gmail.com>
2024-01-04 08:47:38 -0300

    Inicializar valores numéricos a 0

--- BaseDeProjetos/Models/ViewModels/ParticipacaoTotalViewModel.cs
+++ BaseDeProjetos/Models/ViewModels/ParticipacaoTotalViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace BaseDeProjetos.Models.ViewModels
@@ -7,24 +7,24 @@ namespace BaseDeProjetos.Models.ViewModels
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public Usuario Lider { get; set; }
-        public decimal ValorTotalProspeccoes { get; set; }
-        public decimal ValorMedioProspeccoes { get; set; }
-        public int QuantidadeProspeccoes { get; set; }
-        public int QuantidadeProspeccoesLider { get; set; }
-        public decimal TaxaConversaoProposta { get; set; }
+        public decimal ValorTotalProspeccoes { get; set; } = 0;
+        public decimal ValorMedioProspeccoes { get; set; } = 0;
+        public int QuantidadeProspeccoes { get; set; } = 0;
+        public int QuantidadeProspeccoesLider { get; set; } = 0;
+        public decimal TaxaConversaoProposta { get; set; } = 0;
         public List<ParticipacaoViewModel> Participacoes { get; set; }
-        public int QuantidadeProjetos { get; set; }
-        public decimal ValorTotalProspeccoesComProposta { get; set; }
-        public decimal ValorMedioProspeccoesComProposta { get; set; }
-        public decimal ValorTotalProspeccoesConvertidas { get; set; }
-        public decimal ValorMedioProspeccoesConvertidas { get; set; }
+        public int QuantidadeProjetos { get; set; } = 0;
+        public decimal ValorTotalProspeccoesComProposta { get; set; } = 0;
+        public decimal ValorMedioProspeccoesComProposta { get; set; } = 0;
+        public decimal ValorTotalProspeccoesConvertidas { get; set; } = 0;
+        public decimal ValorMedioProspeccoesConvertidas { get; set; } = 0;
         public decimal QuantidadeProspeccoesComProposta { get; set; }
-        public int QuantidadeProspeccoesMembro { get; set; }
-        public decimal TaxaConversaoProjeto { get; set; }
-        public decimal QuantidadeProspeccoesProjeto { get; set; }
-        public decimal AssertividadePrecificacao { get; set; }
-        public decimal FatorContribuicaoFinanceira { get; set; }
-        public decimal MediaFatores { get; set; }
+        public int QuantidadeProspeccoesMembro { get; set; } = 0;
+        public decimal TaxaConversaoProjeto { get; set; } = 0;
+        public decimal QuantidadeProspeccoesProjeto { get; set; } = 0;
+        public decimal AssertividadePrecificacao { get; set; } = 0;
+        public decimal FatorContribuicaoFinanceira { get; set; } = 0;
+        public decimal MediaFatores { get; set; } = 0;
         public Dictionary<string, decimal> RankPorIndicador { get; set; }
         public Dictionary<string, decimal> ValoresMinMedMax { get; set; }
         public List<string> Labels { get; set; }
