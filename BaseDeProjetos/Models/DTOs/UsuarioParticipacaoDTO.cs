5a2e440c4a127b0dca44e9cd1fb125f515a1dfe9
hhenriques1999 <hhenriques1999@gmail.com>
2024-01-05 15:56:18 -0300

    Criar DTO para Usuario e Participacao

---
+++ BaseDeProjetos/Models/DTOs/UsuarioParticipacaoDTO.cs
@@ -0,0 +1,27 @@
+﻿using BaseDeProjetos.Models.Enums;
+
+namespace BaseDeProjetos.Models.DTOs
+{
+    public class UsuarioParticipacaoDTO
+    {
+        public string Id { get; set; }
+        public Instituto Casa { get; set; }
+        public Cargo Cargo { get; set; }
+        public Nivel Nivel { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string Email { get; set; }
+
+        public Usuario ToUsuario()
+        {
+            return new Usuario
+            {
+                Id = Id,
+                Casa = Casa,
+                Cargo = Cargo,
+                Nivel = Nivel,
+                EmailConfirmed = EmailConfirmed,
+                Email = Email
+            };
+        }
+    }
+}
