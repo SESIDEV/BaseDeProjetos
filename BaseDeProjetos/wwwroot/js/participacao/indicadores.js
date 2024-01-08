a1773882b4601662481ac14fda36516a4e1f2ad2
hhenriques1999 <hhenriques1999@gmail.com>
2024-01-08 15:51:57 -0300

    Permitir a formatação dos dados através de funções JS para a tabela de indicadores

--- BaseDeProjetos/wwwroot/js/participacao/indicadores.js
+++ BaseDeProjetos/wwwroot/js/participacao/indicadores.js
@@ -17,7 +17,7 @@ function criarTableDataComClasseCell(conteudo) {
     return tableData
 }
 
-function criarLinhaPesquisadorIndicador(indicador) {
+function criarLinhaPesquisadorIndicador(indicador, tipoDado) {
     let pesquisador = indicador["Pesquisador"]["Email"];
     let valor = indicador["Valor"];
     let tableRow = document.createElement("tr");
@@ -29,7 +29,27 @@ function criarLinhaPesquisadorIndicador(indicador) {
     return tableRow;
 }
 
-function popularTabelaIndicadores(dadosIndicador, nomeIndicador) {
+function formatarValor(tipoDado, indicador) {
+    const formatadorFinanceiro = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
+
+    if (tipoDado == 'i') {
+        return indicador["Valor"];
+    } else if (tipoDado == 'F') {
+        return formatadorFinanceiro.format(indicador["Valor"]);
+    } else if (tipoDado == 'f') {
+        return indicador["Valor"].toFixed(2);
+    } else if (tipoDado == 'p') {
+        console.log(indicador["Valor"]);
+        return (indicador["Valor"] * 100).toFixed(2) + "%";
+    } else {
+        return '';
+    }
+}
+
+function popularTabelaIndicadores(dadosIndicador, nomeIndicador, tipoDado) {
     let corpoTabela = document.querySelector("#corpoTabela");
     let tituloIndicador = document.querySelector("#tituloIndicador");
 
@@ -49,16 +69,16 @@ function popularTabelaIndicadores(dadosIndicador, nomeIndicador) {
     tituloIndicador.innerText = "Carregando indicador..."; 
     dadosIndicador.forEach(indicador => {
         if (indicador) {
-            corpoTabela.appendChild(criarLinhaPesquisadorIndicador(indicador));
+            corpoTabela.appendChild(criarLinhaPesquisadorIndicador(indicador, tipoDado));
         }
     });
     tituloIndicador.innerText = `Indicador: ${nomeIndicador}`;
 }
 
-async function puxarIndicador(identificadorIndicador, nomeIndicador) {
+async function puxarIndicador(identificadorIndicador, nomeIndicador, tipoDado) {
     try {
         let dadosIndicador = await puxarDados(identificadorIndicador);
-        popularTabelaIndicadores(dadosIndicador, nomeIndicador);
+        popularTabelaIndicadores(dadosIndicador, nomeIndicador, tipoDado);
     } catch (error) {
         console.error(error);
     }
