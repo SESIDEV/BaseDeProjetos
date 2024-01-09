fde1ca5b102ec08ed3c72612a34efd888588dd9c
hhenriques1999 <hhenriques1999@gmail.com>
2024-01-09 15:22:07 -0300

    Stub rankings medios na tabela de pesquisadores

--- BaseDeProjetos/Controllers/ParticipacaoController.cs
+++ BaseDeProjetos/Controllers/ParticipacaoController.cs
@@ -240,9 +240,14 @@ namespace BaseDeProjetos.Controllers
                     usuario = await _context.Users.Where(u => u.Id == idUsuario).FirstOrDefaultAsync();
                 }
 
-                _prospeccoes = await _cache.GetCachedAsync("Prospeccoes:Participacao", () => _context.Prospeccao.Include(p => p.Usuario).Include(p => p.Empresa).ToListAsync());
+
+                var participacoes = await GetParticipacoesTotaisUsuarios(new DateTime(2021, 01, 01), new DateTime(DateTime.Now.Year, 12, 31));
+                ObterRankingsMedios(participacoes);
+
+                _prospeccoes = await _cache.GetCachedAsync("Prospeccoes:Participacao", () => _context.Prospeccao.Include(p => p.Usuario).Include(p => p.Empresa).Include(p => p.Status).ToListAsync());
 
                 var participacao = await GetParticipacaoTotalUsuario(usuario, new DateTime(2021, 01, 01), new DateTime(DateTime.Now.Year, 12, 31));
+
 
                 if (participacao != null)
                 {
