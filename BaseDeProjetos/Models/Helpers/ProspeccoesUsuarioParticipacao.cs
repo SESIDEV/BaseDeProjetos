c005157eb3ce9a6f42760df03dabc4a98fff5141
hhenriques1999 <hhenriques1999@gmail.com>
2024-01-12 15:40:38 -0300

    Adicionar mais prospeccoes no ProspeccoesUsuarioParticipacao
    Isso deveria ser um DTO?

--- BaseDeProjetos/Models/Helpers/ProspeccoesUsuarioParticipacao.cs
+++ BaseDeProjetos/Models/Helpers/ProspeccoesUsuarioParticipacao.cs
@@ -7,5 +7,8 @@ namespace BaseDeProjetos.Models.Helpers
         public List<Prospeccao> ProspeccoesTotais { get; set; }
         public List<Prospeccao> ProspeccoesLider { get; set; }
         public List<Prospeccao> ProspeccoesMembro { get; set; }
+        public List<Prospeccao> ProspeccoesTotaisConvertidas { get; internal set; }
+        public List<Prospeccao> ProspeccoesTotaisComProposta { get; internal set; }
+        public object ProspeccoesLiderConvertidas { get; internal set; }
     }
 }
