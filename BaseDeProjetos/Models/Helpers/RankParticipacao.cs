e1ed6cf758dd55fd253330bb9d839e916a4bd763
hhenriques1999 <hhenriques1999@gmail.com>
2024-01-12 14:11:41 -0300

    Renomear ProspeccoesProjeto para ProspeccoesConvertidas

--- BaseDeProjetos/Models/Helpers/RankParticipacao.cs
+++ BaseDeProjetos/Models/Helpers/RankParticipacao.cs
@@ -6,7 +6,7 @@
         public decimal RankValorTotalProspeccoesConvertidas { get; set; }
         public decimal RankValorMedioProspeccoesComProposta { get; set; }
         public decimal RankValorMedioProspeccoesConvertidas { get; set; }
-        public decimal RankQuantidadeProspeccoesProjeto { get; set; }
+        public decimal RankQuantidadeProspeccoesConvertidas { get; set; }
         public decimal RankQuantidadeProspeccoes { get; set; }
         public decimal RankQuantidadeProspeccoesLider { get; set; }
         public decimal RankQuantidadeProspeccoesMembro { get; set; }
