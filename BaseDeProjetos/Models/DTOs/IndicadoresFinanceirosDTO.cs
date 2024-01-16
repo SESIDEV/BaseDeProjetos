7feb375de30a222911f8ba46f7219eec50b5fbf9
hhenriques1999 <hhenriques1999@gmail.com>
2024-01-16 13:29:54 -0300

    Criar DTO para indicadores financeiros a fim de otimização e simplificação de código

---
+++ BaseDeProjetos/Models/DTOs/IndicadoresFinanceirosDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BaseDeProjetos.Controllers
+{
+    internal class IndicadoresFinanceirosDTO
+    {
+        public decimal Despesa { get; set; }
+        public DateTime Data { get; set; }
+        public int QtdPesquisadores { get; set; }
+    }
+}
