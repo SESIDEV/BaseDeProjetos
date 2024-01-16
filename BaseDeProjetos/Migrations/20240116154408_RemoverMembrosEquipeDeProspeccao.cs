5d438f548f5872260e3ae885ef085d2acce8fa20
hhenriques1999 <hhenriques1999@gmail.com>
2024-01-16 12:46:42 -0300

    Remover membros equipe do DB

---
+++ BaseDeProjetos/Migrations/20240116154408_RemoverMembrosEquipeDeProspeccao.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseDeProjetos.Migrations
+{
+    public partial class RemoverMembrosEquipeDeProspeccao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MembrosEquipe",
+                table: "Prospeccao");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MembrosEquipe",
+                table: "Prospeccao",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+    }
+}
