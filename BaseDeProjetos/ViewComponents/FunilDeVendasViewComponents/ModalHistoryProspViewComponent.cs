7f6325d1c089353e41601117f16618a56b6259b7
hhenriques1999 <hhenriques1999@gmail.com>
2024-01-17 11:06:57 -0300

    Usar cache no ModalHistoryProspViewComponent

--- BaseDeProjetos/ViewComponents/FunilDeVendasViewComponents/ModalHistoryProspViewComponent.cs
+++ BaseDeProjetos/ViewComponents/FunilDeVendasViewComponents/ModalHistoryProspViewComponent.cs
@@ -7,15 +7,17 @@ namespace BaseDeProjetos.ViewComponents.FunilDeVendasViewComponents
     public class ModalHistoryProspViewComponent : ViewComponent
     {
         private readonly ApplicationDbContext _context;
+        private readonly DbCache _cache;
 
-        public ModalHistoryProspViewComponent(ApplicationDbContext context)
+        public ModalHistoryProspViewComponent(ApplicationDbContext context, DbCache cache)
         {
             _context = context;
+            _cache = cache;
         }
 
         public async Task<IViewComponentResult> InvokeAsync(string id)
         {
-            var model = await _context.Prospeccao.FindAsync(id);
+            var model = await _cache.GetCachedAsync($"Prospeccao:{id}", () => _context.Prospeccao.FindAsync(id).AsTask());
             return View(model);
         }
     }
