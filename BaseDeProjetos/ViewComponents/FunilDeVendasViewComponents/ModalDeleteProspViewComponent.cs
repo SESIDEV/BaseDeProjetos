42c780cd8dff9e77b51bbcff0ac85b9c98905c4b
hhenriques1999 <hhenriques1999@gmail.com>
2024-01-17 11:03:26 -0300

    Usar cache no ModalDeleteProspViewComponent

--- BaseDeProjetos/ViewComponents/FunilDeVendasViewComponents/ModalDeleteProspViewComponent.cs
+++ BaseDeProjetos/ViewComponents/FunilDeVendasViewComponents/ModalDeleteProspViewComponent.cs
@@ -7,15 +7,18 @@ namespace BaseDeProjetos.ViewComponents.FunilDeVendasViewComponents
     public class ModalDeleteProspViewComponent : ViewComponent
     {
         private readonly ApplicationDbContext _context;
+        private readonly DbCache _cache;
 
-        public ModalDeleteProspViewComponent(ApplicationDbContext context)
+
+        public ModalDeleteProspViewComponent(ApplicationDbContext context, DbCache cache)
         {
             _context = context;
+            _cache = cache;
         }
 
         public async Task<IViewComponentResult> InvokeAsync(string id)
         {
-            var model = await _context.Prospeccao.FindAsync(id);
+            var model = await _cache.GetCachedAsync($"Prospeccao:{id}", () => _context.Prospeccao.FindAsync(id).AsTask());
             return View(model);
         }
     }
