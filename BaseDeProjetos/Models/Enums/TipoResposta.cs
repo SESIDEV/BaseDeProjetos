0bb57bcf0ce2982bdffbeb33ad8c0872be3151ba
crfmattos <gabrielmbserra@gmail.com>
2024-01-30 14:38:50 -0300

    criando enum para os tipos de Respostas

---
+++ BaseDeProjetos/Models/Enums/TipoResposta.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseDeProjetos.Models.Enums
+{
+    public enum TipoResposta
+    {
+        [Display(Name ="Objetiva Simples")]
+        objetivaSimples,
+
+        [Display(Name ="Objetiva Múltipla")]
+        objetivaMultipla,
+
+        [Display(Name ="Descritiva")]
+        discursiva
+    }
+}
