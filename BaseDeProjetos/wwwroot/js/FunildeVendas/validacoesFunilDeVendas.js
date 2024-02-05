09b141dbaf75038d87b2a4ed0b5d42831fe5053a
hhenriques1999 <hhenriques1999@gmail.com>
2024-02-05 14:56:43 -0300

    Simplificar switcheroo de classes usando o verificarInput do utils

--- BaseDeProjetos/wwwroot/js/FunildeVendas/validacoesFunilDeVendas.js
+++ BaseDeProjetos/wwwroot/js/FunildeVendas/validacoesFunilDeVendas.js
@@ -19,53 +19,25 @@ function validarDataContatoInicial(dataContatoInicial) {
 function verificarCampoEmpresa(selectEmpresa) {
     let validacaoEmpresa = validarSelectEmpresa(selectEmpresa);
 
-    if (validacaoEmpresa === true) {
-        selectEmpresa.classList.remove("is-invalid");
-        selectEmpresa.classList.add("is-valid");
-    }
-    else {
-        selectEmpresa.classList.remove("is-valid");
-        selectEmpresa.classList.add("is-invalid");
-    }
+    verificarInput(validacaoEmpresa, selectEmpresa);
 }
 
 function verificarAnotacoesProspeccao(contatoInicialAnotacoes) {
     let validacaoContatoInicialAnotacoes = validarAnotacoesContatoInicial(contatoInicialAnotacoes);
 
-    if (validacaoContatoInicialAnotacoes === true) {
-        contatoInicialAnotacoes.classList.remove("is-invalid");
-        contatoInicialAnotacoes.classList.add("is-valid");
-    }
-    else {
-        contatoInicialAnotacoes.classList.add("is-invalid");
-        contatoInicialAnotacoes.classList.remove("is-valid");
-    }
+    verificarInput(validacaoContatoInicialAnotacoes, contatoInicialAnotacoes);
 }
 
 function verificarCampoNomeProspeccao(nomeProspeccao) {
     let validacaoNomeProspeccao = validarNomeProspeccao(nomeProspeccao);
 
-    if (validacaoNomeProspeccao === true) {
-        nomeProspeccao.classList.remove("is-invalid");
-        nomeProspeccao.classList.add("is-valid");
-    }
-    else {
-        nomeProspeccao.classList.remove("is-valid");
-        nomeProspeccao.classList.add("is-invalid");
-    }
+    verificarInput(validacaoNomeProspeccao, nomeProspeccao);
 }
 
 function verificarCampoContatoInicial(dataContatoInicial) {
     let validacaoContatoInicial = validarDataContatoInicial(dataContatoInicial);
 
-    if (validacaoContatoInicial === true) {
-        dataContatoInicial.classList.add("is-valid");
-        dataContatoInicial.classList.remove("is-invalid");
-    }
-    else {
-        dataContatoInicial.classList.remove("is-valid");
-        dataContatoInicial.classList.add("is-invalid");
-    }
+    verificarInput(validacaoContatoInicial, dataContatoInicial)
 }
 
 function validarAnotacoesContatoInicial(contatoInicialAnotacoes) {
