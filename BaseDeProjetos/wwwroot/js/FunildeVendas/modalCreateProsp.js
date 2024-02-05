1d88af56aff69f0f91243a333d83e9e51603cb61
hhenriques1999 <hhenriques1999@gmail.com>
2024-02-05 13:46:29 -0300

    Incluir event listeners para as lógicas de validação do modal do Funil

--- BaseDeProjetos/wwwroot/js/FunildeVendas/modalCreateProsp.js
+++ BaseDeProjetos/wwwroot/js/FunildeVendas/modalCreateProsp.js
@@ -1,83 +1,5 @@
-﻿function validarNomeProspeccao() {
-    let nomeProspeccao = document.getElementById("NomeProspeccao");
-    return nomeProspeccao.value && nomeProspeccao.value.length > 3;
-}
-
-
-function validarSelectEmpresa() {
-    let selectEmpresa = document.getElementById("EmpresaId");
-    return selectEmpresa.value && selectEmpresa.value !== "-1";
-}
-
-function validarDataContatoInicial() {
-    let dataContatoInicial = document.getElementById("Status_0__Data");
-    if (dataContatoInicial.value) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-function verificarCampoEmpresa() {
-    let selectEmpresa = document.getElementById("EmpresaId");
-    let validacaoEmpresa = validarSelectEmpresa();
-
-    if (validacaoEmpresa === true) {
-        selectEmpresa.classList.remove("is-invalid");
-        selectEmpresa.classList.add("is-valid");
-    }
-    else {
-        selectEmpresa.classList.remove("is-valid");
-        selectEmpresa.classList.add("is-invalid");
-    }
-}
-
-function verificarAnotacoesProspeccao() {
-    let contatoInicialAnotacoes = document.getElementById("Status_0__Anotacoes");
-    let validacaoContatoInicialAnotacoes = validarAnotacoesContatoInicial();
-
-    if (validacaoContatoInicialAnotacoes === true) {
-        contatoInicialAnotacoes.classList.remove("is-invalid");
-        contatoInicialAnotacoes.classList.add("is-valid");
-    }
-    else {
-        contatoInicialAnotacoes.classList.add("is-invalid");
-        contatoInicialAnotacoes.classList.remove("is-valid");
-    }
-}
-
-function verificarCampoNomeProspeccao() {
-    let nomeProspeccao = document.getElementById("NomeProspeccao");
-    let validacaoNomeProspeccao = validarNomeProspeccao();
-
-    if (validacaoNomeProspeccao === true) {
-        nomeProspeccao.classList.remove("is-invalid");
-        nomeProspeccao.classList.add("is-valid");
-    }
-    else {
-        nomeProspeccao.classList.remove("is-valid");
-        nomeProspeccao.classList.add("is-invalid");
-    }
-}
-
-function verificarCampoContatoInicial() {
-    let dataContatoInicial = document.getElementById("Status_0__Data");
-    let validacaoContatoInicial = validarDataContatoInicial();
-
-    if (validacaoContatoInicial === true) {
-        dataContatoInicial.classList.add("is-valid");
-        dataContatoInicial.classList.remove("is-invalid");
-    }
-    else {
-        dataContatoInicial.classList.remove("is-valid");
-        dataContatoInicial.classList.add("is-invalid");
-    }
-}
-
-function validarAnotacoesContatoInicial() {
-    let contatoInicialAnotacoes = document.getElementById("Status_0__Anotacoes");
-    return contatoInicialAnotacoes.value && contatoInicialAnotacoes.value > 3;
+﻿function validarAnotacoesContatoInicial(contatoInicialAnotacoes) {
+    return contatoInicialAnotacoes.value && contatoInicialAnotacoes.value.length > 3;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -88,33 +10,29 @@ document.addEventListener('DOMContentLoaded', () => {
     let dataContatoInicial = document.getElementById("Status_0__Data");
     let contatoInicialAnotacoes = document.getElementById("Status_0__Anotacoes");
 
-    //const concluirCreateProsp = document.getElementById("concluirCreateProsp");
-
-    /*    concluirCreateProsp.addEventListener("click", (event) => validarCampos(event));*/
-
     if (selectEmpresa) {
-        selectEmpresa.addEventListener("change", verificarCampoEmpresa);
+        selectEmpresa.addEventListener("change", () => verificarCampoEmpresa(selectEmpresa));
     }
     else {
         console.error(`No select emepresa: ${selectEmpresa}`);
     }
 
     if (nomeProspeccao) {
-        nomeProspeccao.addEventListener("change", verificarCampoNomeProspeccao);
+        nomeProspeccao.addEventListener("change", () => verificarCampoNomeProspeccao(nomeProspeccao));
     }
     else {
         console.error(`No nomeProspeccao: ${nomeProspeccao}`);
     }
 
     if (dataContatoInicial) {
-        dataContatoInicial.addEventListener("change", verificarCampoContatoInicial);
+        dataContatoInicial.addEventListener("change", () => verificarCampoContatoInicial(dataContatoInicial));
     }
     else {
         console.error(`No dataContatoInicial: ${dataContatoInicial}`);
     }
 
     if (contatoInicialAnotacoes) {
-        contatoInicialAnotacoes.addEventListener("change", validarAnotacoesContatoInicial);
+        contatoInicialAnotacoes.addEventListener("change", () => verificarAnotacoesProspeccao(contatoInicialAnotacoes));
     }
     else {
         console.error(`No contatoInicialAnotacoes: ${contatoInicialAnotacoes}`);
---
+++ BaseDeProjetos/wwwroot/js/FunildeVendas/modalEditProsp.js
@@ -0,0 +1,23 @@
+﻿
+document.addEventListener('DOMContentLoaded', () => {
+    let listNomeProspeccao = document.querySelectorAll("#NomeProspeccao");
+    let listSelectEmpresa = document.querySelectorAll("#EmpresaId");
+
+    listSelectEmpresa.forEach((selectEmpresa) => {
+        if (selectEmpresa) {
+            selectEmpresa.addEventListener("change", () => verificarCampoEmpresa(selectEmpresa));
+        }
+        else {
+            console.error(`No select empresa: ${selectEmpresa}`);
+        }
+    });
+
+    listNomeProspeccao.forEach((nomeProspeccao) => {
+        if (nomeProspeccao) {
+            nomeProspeccao.addEventListener("change", () => verificarCampoNomeProspeccao(nomeProspeccao));
+        }
+        else {
+            console.error(`No nomeProspeccao: ${nomeProspeccao}`);
+        }
+    });
+});
