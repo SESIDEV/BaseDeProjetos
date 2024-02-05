9f070f9043b1fcf5e05fb3e97fcf2e1568f62839
hhenriques1999 <hhenriques1999@gmail.com>
2024-02-05 14:56:26 -0300

    Adicionar validação para os modais de criar cargo

---
+++ BaseDeProjetos/wwwroot/js/Cargos/modalCreateCargo.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    let nomeCargo = document.querySelector("#Nome");
+    let horas = document.querySelector("#HorasSemanais");
+    let salario = document.querySelector("#Salario");
+
+    adicionarListenerVerificacao(nomeCargo, verificarNomeCargo)
+
+    adicionarListenerVerificacao(horas, verificarHoras);
+
+    adicionarListenerVerificacao(salario, verificarSalario)
+});
---
+++ BaseDeProjetos/wwwroot/js/Cargos/validacoesCargos.js
@@ -0,0 +1,29 @@
+﻿function validarNomeCargo(nomeCargo) {
+    return nomeCargo.value && nomeCargo.value.length > 3;
+}
+
+function verificarNomeCargo(nomeCargo) {
+    let validacaoNomeCargo = validarNomeCargo(nomeCargo);
+
+    verificarInput(validacaoNomeCargo, nomeCargo);
+}
+
+function validarHoras(horas) {
+    return horas.value && parseInt(horas.value) > 0;
+}
+
+function verificarHoras(horas) {
+    let validacaoHoras = validarHoras(horas);
+
+    verificarInput(validacaoHoras, horas);
+}
+
+function validarSalario(salario) {
+    return salario.value && parseFloat(salario.value) > 0;
+}
+
+function verificarSalario(salario) {
+    let validacaoSalario = validarSalario(salario);
+
+    verificarInput(validacaoSalario, salario);
+}
