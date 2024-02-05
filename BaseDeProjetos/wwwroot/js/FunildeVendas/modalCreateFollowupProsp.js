4fb7755f2494cc25f71443845060831fcdf265ed
hhenriques1999 <hhenriques1999@gmail.com>
2024-02-05 14:05:23 -0300

    Adicionar validação para as anotação de criar followup

---
+++ BaseDeProjetos/wwwroot/js/FunildeVendas/modalCreateFollowupProsp.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    let anotacoes = document.querySelectorAll("#Anotacoes");
+
+    anotacoes.forEach(anotacao => {
+        if (anotacao) {
+            anotacao.addEventListener("change", () => verificarAnotacoesProspeccao(anotacao));
+        }
+        else {
+            console.error(`No anotacao: ${anotacao}`);
+        }
+    });
+});
