01244a2c260d5fe2472d0437154f30333f6f0c9f
hhenriques1999 <hhenriques1999@gmail.com>
2024-02-06 13:53:41 -0300

    Stub de funções para validação de projeto

---
+++ BaseDeProjetos/wwwroot/js/projetos/validacoesProjetos.js
@@ -0,0 +1,28 @@
+﻿function validarNomeProjeto(nomeProjeto) {
+    return nomeProjeto.value && nomeProjeto.value.length > 3;
+}
+
+function validarProponente(proponente) {
+    return proponente.value && proponente.value != "";
+}
+
+// TODO: Parse date?
+function validarDataFim(dataInicio, dataFim) {
+    return dataFim.value > dataInicio.value;
+}
+
+function validarDuracaoMeses(duracaoMeses) {
+    return parseInt(duracaoMeses.value) > 0;
+}
+
+function validarCustoHM(custoHM) {
+    return parseFloat(custoHM.value) > 0;
+}
+
+function validarValorProjeto(valorProjeto) {
+    return parseFloat(valorProjeto.value) > 0;
+}
+
+function validarSelectEmpresaProjetos(selectEmpresa) {
+    return selectEmpresa.value && selectEmpresa.value != "-1" && selectEmpresa.value != "";
+}
