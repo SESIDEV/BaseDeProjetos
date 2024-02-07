a0d924eadc730c86259ea2931cb4135fb712f2d4
hhenriques1999 <hhenriques1999@gmail.com>
2024-02-07 09:03:26 -0300

    Adicionar ; faltante

--- BaseDeProjetos/wwwroot/js/Cargos/modalCreateCargo.js
+++ BaseDeProjetos/wwwroot/js/Cargos/modalCreateCargo.js
@@ -3,7 +3,7 @@
     let horas = document.querySelector("#HorasSemanais");
     let salario = document.querySelector("#Salario");
 
-    adicionarListenerVerificacao(nomeCargo, validarNomeCargo)
+    adicionarListenerVerificacao(nomeCargo, validarNomeCargo);
     adicionarListenerVerificacao(horas, validarHoras);
-    adicionarListenerVerificacao(salario, validarSalario)
+    adicionarListenerVerificacao(salario, validarSalario);
 });
