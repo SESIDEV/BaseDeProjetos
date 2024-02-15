ed73950954ba1e8c726e9f88eb82cb2d7e8229b3
crfmattos <gabrielmbserra@gmail.com>
2024-02-15 13:58:56 -0300

    adicionar btn de adicionar input,  deletar div e adicionar checkbox

--- BaseDeProjetos/Models/Formulario/PerguntasForm.cs
+++ BaseDeProjetos/Models/Formulario/PerguntasForm.cs
@@ -1,4 +1,5 @@
 ﻿using BaseDeProjetos.Models.Enums;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace BaseDeProjetos.Models.Formulario
---
+++ BaseDeProjetos/wwwroot/js/Formulario/formulario.js
@@ -0,0 +1,21 @@
+﻿class Perguntas {
+    constructor(tituloPergunta, tipo, obrigatorio) {
+        this.tituloPergunta = tituloPergunta
+        this.tipo = tipo
+        this.obrigatorio = obrigatorio
+    }
+}
+
+let listaPerguntas = []
+
+function CriarPergunta(tipo, pergunta, obrigatorio) {
+    let perguntas = new Perguntas(tipo, pergunta, obrigatorio)
+}
+
+function AdicionarPergunta(pergunta) {
+    listaPerguntas.push(pergunta)
+}
+
+function SerializarPerguntas(perguntas) {
+    Json.stringify(perguntas)
+}
