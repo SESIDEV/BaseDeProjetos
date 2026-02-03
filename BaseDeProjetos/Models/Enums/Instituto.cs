830ee1073885177c87c1c1422cf634cddd8a4063
Larissa Gabriela Oliveira de Santana <larissagosantana@gmail.com>
2026-02-03 16:09:56 -0300

    - Incluí CBT - ISIBF; CIN - ISIBF; CPQ - ISIBF; CEP - ISIBF; CSQ - ISIBF e Serviços - GPD

--- BaseDeProjetos/Models/Enums/Instituto.cs
+++ BaseDeProjetos/Models/Enums/Instituto.cs
@@ -7,16 +7,34 @@ namespace BaseDeProjetos.Models.Enums
         [Display(Name = "ISI - Química Verde")]
         ISIQV,
 
-        [Display(Name = "ISI - Inspeção & Integridade")]
-        ISIII,
-
         [Display(Name = "CIS - Saúde Ocupacional")]
         CISHO,
+
+        [Display(Name = "ISIBF - [CBT] Biossintéticos e Fibras")]
+        ISIBF_CBT,
+
+        [Display(Name = "ISIBF - [CIN] Biossintéticos e Fibras")]
+        ISIBF_CIN,
+
+        [Display(Name = "ISIBF - [CPQ] Biossintéticos e Fibras")]
+        ISIBF_CPQ,
+
+        [Display(Name = "ISIBF - [CEP] Biossintéticos e Fibras")]
+        ISIBF_CEP,
+
+        [Display(Name = "ISIBF - [CSQ] Biossintéticos e Fibras")]
+        ISIBF_CSQ,
+
+        [Display(Name = "GPD - Serviços")] // Gerência de Pesquisa e Desenvolvimento
+        Serviços_GPD,
 
         [Display(Name = "Supervisão")]
         Super,
 
         [Display(Name = "ISI - Sistemas Virtuais de Produção")]
-        ISISVP
+        ISISVP,
+
+        [Display(Name = "ISI - Inspeção & Integridade")]
+        ISIII
     }
 }
